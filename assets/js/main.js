16261f54bc4693e1af4526ba09d3886c43fbfd8f
Parth Sharma <parthsharma151@gmail.com>
2020-12-02 21:22:50 +0530

    add: setup links, basic cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Medilab - v2.0.0
-* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
